7c53c1211b6766ff656d505d3593e03c98da1394
SohWenMing <wenming.soh@gmail.com>
2024-06-02 09:28:20 +0800

    cleaned up files

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,19 +1,19 @@
 from textnode import TextNode
 
-# class TextNode:
-#     def __init__(self, text, text_type, url=None):
-#         self.text = text
-#         self.text_type = text_type
-#         self.url = url
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
-#     def __eq__(self, other):
-#         if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
-#             return True
-#         else:
-#             return False
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        else:
+            return False
     
-#     def __repr__(self):
-#         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 def checkParentNode(tag, children):
     if tag == None:
