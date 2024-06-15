1196747dddd7256860e093f9008ee8521ebc633e
SohWenMing <wenming.soh@gmail.com>
2024-06-15 17:58:34 +0800

    working on file function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode
+import os
+
+def list_files(current_path):
+    print("current path: ", current_path)
+    paths = []
+    dir_items = os.listdir(current_path)
+    for item in dir_items:
+        full_path = f"{current_path}/{item}"
+        if os.path.isfile(full_path):
+            paths.append(full_path)
+        elif os.path.isdir(full_path):
+            paths.extend(list_files(f"{current_path}/{item}"))
+    return paths
 
 def main():
-    test_node = TextNode("this is a text", "HTML", "www.google.com")
-    print(test_node)
+    print(list_files("."))
 
 
 main()
